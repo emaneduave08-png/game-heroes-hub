5cff0502aa4e20c8b140f2de070fb4466b60a6be
EMANUEL <emaneduave08@gmail.com>
2026-03-28 11:45:34 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-
+function showMLHero() {
+  document.getElementById("mlOutput").innerHTML =
+    "<h2>Alucard</h2>" +
+    "<p>Role: Fighter</p>" +
+    "<p>Strategy: Mag-farm muna, then engage kapag may items na para sa lifesteal sustain.</p>";
+}
