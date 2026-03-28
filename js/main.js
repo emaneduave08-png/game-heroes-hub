130dcc4ce1f3278ed16a1e3774a2331b99f03915
EMANUEL <emaneduave08@gmail.com>
2026-03-28 11:46:18 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,3 +4,9 @@ function showMLHero() {
     "<p>Role: Fighter</p>" +
     "<p>Strategy: Mag-farm muna, then engage kapag may items na para sa lifesteal sustain.</p>";
 }
+function showHOKHero() {
+  document.getElementById("hokOutput").innerHTML =
+    "<h2>Lu Bu</h2>" +
+    "<p>Role: Fighter</p>" +
+    "<p>Strategy: Mag-engage kapag may ult, very strong sa 1v1 at team fights.</p>";
+}
